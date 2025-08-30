5c39890c89ae17d7f616ac4ef5cffd5a4b73265a
Dave Chavez <chavezdave.a@gmail.com>
2025-08-30 09:14:01 +0800

    forcing to run @ https

--- app.js
+++ app.js
@@ -7,6 +7,15 @@ require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+//force to run https
+
+app.use((req, res, next) => {
+  if (req.headers['x-forwarded-proto'] !== 'https' && process.env.NODE_ENV === 'production') {
+    return res.redirect('https://' + req.headers.host + req.url);
+  }
+  next();
+});
+
 // Setting up template engine
 app.set("view engine", "ejs");
 app.use(express.static("public"));
@@ -134,12 +143,3 @@ function formatFileSize(bytes) {
   if (!bytes) return null;
   return (bytes / (1024 * 1024)).toFixed(2) + " MB";
 }
-
-//force to run https
-
-app.use((req, res, next) => {
-  if (req.headers['x-forwarded-proto'] !== 'https' && process.env.NODE_ENV === 'production') {
-    return res.redirect('https://' + req.headers.host + req.url);
-  }
-  next();
-});
